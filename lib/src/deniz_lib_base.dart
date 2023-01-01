0ef79822ebf36589f6b3b323110acfebb02f2d88
Amin <m.aminwhat@gmail.com>
2023-01-01 15:07:21 +0330

    add deniz sockets system

--- lib/src/deniz_lib_base.dart
+++ lib/src/deniz_lib_base.dart
@@ -1,7 +1,9 @@
 import 'package:deniz_lib/src/device/device.dart';
 import 'package:deniz_lib/src/init/init.dart';
 import 'package:deniz_lib/src/router/router.dart';
+import 'package:deniz_lib/src/sockets/sockets.dart';
 
 typedef DenizDevice = Device;
 typedef DenizInit = Init;
 typedef DenizRouter = Router;
+typedef DenizSockets = Sockets;
---
+++ lib/src/sockets/sockets.dart
@@ -0,0 +1,42 @@
+import 'dart:developer';
+import 'package:flutter/cupertino.dart';
+import 'package:meta/meta.dart';
+import 'package:socket_io_client/socket_io_client.dart';
+
+class Sockets {
+  late Socket _socket;
+
+  String connectionName = 'Deniz-Sockets';
+
+  factory Sockets.on(Sockets readySocket) {
+    readySocket.connectSocket();
+    return readySocket;
+  }
+
+  @mustCallSuper
+  void connectSocket() {
+    onConnect();
+    onConnectError();
+    onDisConnect();
+  }
+
+  @protected
+  @mustCallSuper
+  void onConnect() {
+    _socket.onConnect((data) => log('$connectionName: Connection Established'));
+  }
+
+  @protected
+  @mustCallSuper
+  void onConnectError() {
+    _socket.onConnectError(
+        (data) => log('$connectionName: Connection Error: $data'));
+  }
+
+  @protected
+  @mustCallSuper
+  void onDisConnect() {
+    _socket.onDisconnect(
+        (data) => log('$connectionName: Socket.IO Server Disconnected'));
+  }
+}
