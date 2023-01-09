f4364b24276d313afe73d69f8d3026a69bd53464
Amin <m.aminwhat@gmail.com>
2023-01-09 12:15:09 +0330

    change router to const class

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -6,28 +6,27 @@ class Router {
   final List<String> routes;
   final List<Widget> screens;
   final Widget pageNotFound;
+  final int initialIndex;
 
-  late Map<String, Widget> _matchedScreens;
-
-  Router({
+  const Router({
     required this.screens,
     required this.routes,
     this.pageNotFound = const PageNotFound(),
-  }) {
-    if (routes.length != screens.length) {
-      throw Exception('Routes does not match screens');
-    } else {
-      for (var i = 0; i < routes.length; i++) {
-        _matchedScreens.assign(routes[i], screens[i]);
-      }
-    }
-  }
+    this.initialIndex = 0,
+  })  : assert(initialIndex < routes.length),
+        assert(routes.length != screens.length);
 
-  String get initialRoute => routes[0];
+  String get initialRoute => routes[initialIndex];
 
   Route onGenerateRoute(RouteSettings routeSettings) {
+    Map<String, Widget> matchedScreens = {};
+
+    for (var i = 0; i < routes.length; i++) {
+      matchedScreens.assign(routes[i], screens[i]);
+    }
+
     return GetPageRoute(
-      page: () => _matchedScreens[routeSettings.name] ?? pageNotFound,
+      page: () => matchedScreens[routeSettings.name] ?? pageNotFound,
     );
   }
 }
