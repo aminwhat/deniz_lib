ae986bbca6058d2ea81e3c783dceacf71aa2228b
Amin <m.aminwhat@gmail.com>
2023-02-03 12:53:46 +0330

    add transition

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -1,22 +1,31 @@
 import 'package:deniz_lib/src/router/page_not_found.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 class Router {
   final Map<String, Widget> routes;
   final Widget pageNotFound;
   final int initialIndex;
+  final PageTransitionType? pageTransitionType;
 
   const Router({
     required this.routes,
     this.initialIndex = 0,
     this.pageNotFound = const PageNotFound(),
+    this.pageTransitionType,
   }) : assert(initialIndex <= routes.length);
 
   String get initialRoute => routes[initialIndex]?.key.toString() ?? '/';
 
   Route onGenerateRoute(RouteSettings routeSettings) {
-    return MaterialPageRoute(
-      builder: (context) => routes[routeSettings.name] ?? pageNotFound,
-    );
+    if (pageTransitionType != null) {
+      return PageTransition(
+        child: routes[routeSettings.name] ?? pageNotFound,
+        type: pageTransitionType!,
+      );
+    } else {
+      return MaterialPageRoute(
+          builder: (context) => routes[routeSettings.name] ?? pageNotFound);
+    }
   }
 }
