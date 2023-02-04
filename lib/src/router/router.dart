03a506762bed0a12b9d37049426e7a5f0dda884f
Amin <m.aminwhat@gmail.com>
2023-02-04 21:49:06 +0330

    try to add custom transition to the router

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -6,26 +6,52 @@ class Router {
   final Map<String, Widget> routes;
   final Widget pageNotFound;
   final int initialIndex;
+
+  /// Try this or transition *[Not Both]*
+  /// for all the screens
   final PageTransitionType? pageTransitionType;
+
+  /// Try this or pageTransitionType *[Not Both]*
+  /// by the up sort you choose for the screens
+  final List<PageTransitionType>? transitions;
 
   const Router({
     required this.routes,
     this.initialIndex = 0,
     this.pageNotFound = const PageNotFound(),
     this.pageTransitionType,
-  }) : assert(initialIndex <= routes.length);
+    this.transitions,
+  })  : assert(initialIndex <= routes.length),
+        assert(
+          (pageTransitionType == null)
+              ? transitions != null
+              // ignore: unnecessary_null_comparison
+              : pageTransitionType != null,
+        );
 
   String get initialRoute => routes[initialIndex]?.key.toString() ?? '/';
 
   Route onGenerateRoute(RouteSettings routeSettings) {
+    Widget pageRoute = routes[routeSettings.name] ?? pageNotFound;
     if (pageTransitionType != null) {
       return PageTransition(
-        child: routes[routeSettings.name] ?? pageNotFound,
+        child: pageRoute,
         type: pageTransitionType!,
       );
+    } else if (transitions != null) {
+      int index = 0;
+      routes.forEach((key, value) {
+        if (value == pageRoute) {
+          return;
+        } else {
+          index++;
+        }
+      });
+      return PageTransition(child: pageRoute, type: transitions![index]);
     } else {
       return MaterialPageRoute(
-          builder: (context) => routes[routeSettings.name] ?? pageNotFound);
+        builder: (context) => pageRoute,
+      );
     }
   }
 }
